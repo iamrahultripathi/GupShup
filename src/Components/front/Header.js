d2f51c92b7b4b426fd0337a51bb571f92c4c254d
Rahul <rahultripathi0027@gmail.com>
2023-05-10 20:03:43 +0530

    Final Commit

---
+++ src/Components/front/Header.js
@@ -0,0 +1,23 @@
+import Button from '../ui/Button';
+import Toaster from '../ui/Toaster';
+import Typography from '../ui/Typography';
+
+import { Context } from '../../Contexts/userContext';
+import { useContext } from 'react';
+export default function Header() {
+    const user = useContext(Context);
+    const HandleLogout = () => {
+        localStorage.removeItem('googleLogin');
+        user.Logout();
+    };
+
+    return ( <div className = 'px-2 py-2 border-b-2 border-gray-100 flex items-center justify-between' > { /* <Toaster type='failure' /> */ } <
+        Typography type = 'secondary'>
+        GupShup 
+        </Typography > 
+        <Button onClick = { HandleLogout } btnType = 'secondary'>
+        Logout 
+        </Button> 
+        </div >
+    );
+}
